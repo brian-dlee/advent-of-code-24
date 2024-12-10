472c9788c8ee210dd3d348b1fd0c3338f3c9489f
Brian Lee <briandl92391@gmail.com>
2024-12-09 23:04:54 -0800

    day 10

---
+++ d10/a.py
@@ -0,0 +1,83 @@
+import logging
+import pathlib
+
+from aoc import grid as g
+from aoc.puzzle_input import read_input_lines_v2
+from aoc.solver import solver
+
+
+def move_up(p: g.Point):
+    return p[0], g.move_up(p[1])
+
+
+def move_right(p: g.Point):
+    return g.move_right(p[0]), p[1]
+
+
+def move_down(p: g.Point):
+    return p[0], g.move_down(p[1])
+
+
+def move_left(p: g.Point):
+    return g.move_left(p[0]), p[1]
+
+
+def find_peaks(grid: g.Grid, position: g.Point) -> set[g.Point]:
+    current_elevation = int(grid.get(position))
+
+    if current_elevation == 9:
+        return set([position])
+
+    next_elevation = current_elevation + 1
+    peaks: set[g.Point] = set()
+
+    p = move_up(position)
+    if grid.is_in_bounds(p):
+        v = int(grid.get(p))
+        if v == next_elevation:
+            peaks = peaks.union(find_peaks(grid, p))
+
+    p = move_right(position)
+    if grid.is_in_bounds(p):
+        v = int(grid.get(p))
+        if v == next_elevation:
+            peaks = peaks.union(find_peaks(grid, p))
+
+    p = move_down(position)
+    if grid.is_in_bounds(p):
+        v = int(grid.get(p))
+        if v == next_elevation:
+            peaks = peaks.union(find_peaks(grid, p))
+
+    p = move_left(position)
+    if grid.is_in_bounds(p):
+        v = int(grid.get(p))
+        if v == next_elevation:
+            peaks = peaks.union(find_peaks(grid, p))
+
+    return peaks
+
+
+def solve(input_file: pathlib.Path) -> str:
+    trailheads: list[g.Point] = []
+
+    rows = list(read_input_lines_v2(input_file))
+    grid = g.Grid(rows)
+
+    for y, row in grid:
+        for x, col in row:
+            if col == "0":
+                trailheads.append((x, y))
+
+    logging.debug(f"{trailheads=}")
+
+    result: int = 0
+
+    for trailhead in trailheads:
+        result += len(find_peaks(grid, trailhead))
+
+    return f"{result=}"
+
+
+if __name__ == "__main__":
+    solver(solve)
